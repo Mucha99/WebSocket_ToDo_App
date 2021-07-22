17bedc6f6c9f9d331b4c22dfbecfefe3fcbfa790
Bartek Mucha <bartekmucha43@gmail.com>
2021-07-22 21:04:38 +0200

    Add server - third stage

--- server.js
+++ server.js
@@ -1,9 +1,29 @@
 const express = require('express');
+const socket = require('socket.io');
+
+const tasks = [];
 
 const app = express();
-app.listen(8000, () => {
+const server = app.listen(process.env.PORT || 8000, () => {
     console.log('Server is running on port: 8000');
 });
+const io = socket(server);
+
+io.on('connection', (socket) => {
+    console.log('New user! Its id - ' + socket.id);
+    socket.on('addTask', (task) => {
+        console.log('Add new task ! ' + socket.io);
+        tasks.push(task);
+        socket.broadcast.emit('addTask', task);
+    })
+    socket.on('removeTask', (taskId) => {
+        console.log('Remove task ' + socket.id);
+        const task = tasks.find((task) => task.id === taskId);
+        const index = tasks.indexOf(task);
+        tasks.splice(index, 1);
+        socket.broadcast.emit('removeTask', taskId)
+    })
+})
 
 app.use((req, res) => {
     res.status(404).send({ message: 'Not found...'})
